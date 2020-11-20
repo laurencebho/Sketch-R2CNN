b98c13334decb2a6df40d8efdeb8c36ecd9d93b7
Laurie <laurence.ho@durham.ac.uk>
2020-11-20 17:40:18 +0000

    download links

--- run.sh
+++ run.sh
@@ -15,3 +15,20 @@ module load cuda/10.1-cudnn7.6
 
 cd scripts/
 python3 quickdraw_r2cnn_get_images.py
+
+CL_MODEL="resnet50"
+CL_DATASET="quickdraw"
+CL_DATASET_ROOT="../quickdraw"
+CL_LOG_DIR="../trained_weights"
+
+CL_CKPT_PREFIX="quickdraw_${CL_MODEL}"
+mkdir "${CL_LOG_DIR}/${CL_CKPT_PREFIX}_eval"
+
+    python quickdraw_r2cnn_eval.py \
+        --checkpoint "/host${CL_LOG_DIR}/${CL_CKPT_PREFIX}" \
+        --dataset_fn ${CL_DATASET} \
+        --dataset_root "/host${CL_DATASET_ROOT}" \
+        --intensity_channels 8 \
+        --log_dir "/host${CL_LOG_DIR}/${CL_CKPT_PREFIX}_get_images" \
+        --model_fn ${CL_MODEL} \
+    2>&1 | tee -a "${CL_LOG_DIR}/${CL_CKPT_PREFIX}_get_images/get_images.log"
